3d57a1618769a4b6b4f3b1f3d64883b623b10df5
XYmar <xqingliuwu@163.com>
2018-04-23 10:15:24 +0800

    设备新增修改弹框

--- src/views/device/device.vue
+++ src/views/device/device.vue
@@ -37,7 +37,7 @@
       </el-table-column>
       <el-table-column align="center" :label="$t('table.actions')" width="280" class-name="small-padding fixed-width">
         <template slot-scope="scope">
-          <el-button type="primary" size="mini">{{$t('table.edit')}}</el-button>
+          <el-button type="primary" size="mini" @click="handleUpdate(scope.row)">{{$t('table.edit')}}</el-button>
           <el-button size="mini" type="success">{{$t('table.publish')}}
           </el-button>
           <el-button size="mini" type="danger">{{$t('table.delete')}}
@@ -87,6 +87,25 @@
       </el-pagination>
     </div>-->
 
+    <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
+      <el-form :rules="rules" ref="dataForm" :model="temp" label-position="left" label-width="70px" style='width: 400px; margin-left:50px;'>
+        <el-form-item :label="$t('table.deviceName')" prop="name">
+          <el-input v-model="temp.name"></el-input>
+        </el-form-item>
+        <el-form-item :label="$t('table.deviceIP')" prop="ip">
+          <el-input v-model="temp.ip"></el-input>
+        </el-form-item>
+        <el-form-item :label="$t('table.devicePath')" prop="path">
+          <el-input v-model="temp.path"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">{{$t('table.cancel')}}</el-button>
+        <el-button v-if="dialogStatus=='create'" type="primary" @click="createData">{{$t('table.confirm')}}</el-button>
+        <el-button v-else type="primary" @click="updateData">{{$t('table.confirm')}}</el-button>
+      </div>
+    </el-dialog>
+
   </div>
 </template>
 
@@ -117,7 +136,26 @@
           },
           sortable: null,
           oldList: [],
-          newList: []
+          newList: [],
+          temp: {
+            name: '',
+            ip: '',
+            path: ''
+          },
+          dialogFormVisible: false,
+          dialogStatus: '',
+          textMap: {
+            update: 'Edit',
+            create: 'Create'
+          },
+          dialogPvVisible: false,
+          pvData: [],
+          rules: {
+            type: [{ required: true, message: 'type is required', trigger: 'change' }],
+            timestamp: [{ type: 'date', required: true, message: 'timestamp is required', trigger: 'change' }],
+            title: [{ required: true, message: 'title is required', trigger: 'blur' }]
+          },
+          downloadLoading: false
         }
       },
       filters: {
@@ -165,6 +203,48 @@
         handleCreate() {
           this.resetTemp()
           this.dialogStatus = 'create'
+          this.dialogFormVisible = true
+          this.$nextTick(() => {
+            this.$refs['dataForm'].clearValidate()
+          })
+        },
+        updateData() {
+          this.$refs['dataForm'].validate((valid) => {
+            if (valid) {
+              const tempData = Object.assign({}, this.temp)
+              tempData.timestamp = +new Date(tempData.timestamp) // change Thu Nov 30 2017 16:41:05 GMT+0800 (CST) to 1512031311464
+              updateArticle(tempData).then(() => {
+                for (const v of this.list) {
+                  if (v.id === this.temp.id) {
+                    const index = this.list.indexOf(v)
+                    this.list.splice(index, 1, this.temp)
+                    break
+                  }
+                }
+                this.dialogFormVisible = false
+                this.$notify({
+                  title: '成功',
+                  message: '更新成功',
+                  type: 'success',
+                  duration: 2000
+                })
+              })
+            }
+          })
+        },
+        createData() {
+          this.$refs['dataForm'].validate((valid) => {
+            if (valid) {
+              this.temp.name = '设备1';
+              this.temp.ip = '192.168.0.111';
+              this.temp.path = 'D:/';
+            }
+          })
+        },
+        handleUpdate(row) {
+          this.temp = Object.assign({}, row) // copy obj
+          this.temp.timestamp = new Date(this.temp.timestamp)
+          this.dialogStatus = 'update'
           this.dialogFormVisible = true
           this.$nextTick(() => {
             this.$refs['dataForm'].clearValidate()
