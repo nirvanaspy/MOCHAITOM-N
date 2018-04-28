2bd76e3a5868c45a28071ddd97c605210e2c8036
XYmar <xqingliuwu@163.com>
2018-04-28 09:49:29 +0800

    去掉request中的超时限制

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,8 +5,7 @@ import { getToken } from '@/utils/auth'
 
 // create an axios instance
 const service = axios.create({
-  baseURL: process.env.BASE_API, // api的base_url
-  timeout: 5000 // request timeout
+  baseURL: process.env.BASE_API
 })
 
 // request interceptor
