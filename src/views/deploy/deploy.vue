90fd34592e908df639f1fc56bee52463fdc89e99
XYmar <xqingliuwu@163.com>
2018-05-29 15:03:53 +0800

    实现部署功能（刷新时会闪）

--- src/views/deploy/deploy.vue
+++ src/views/deploy/deploy.vue
@@ -44,14 +44,15 @@
             <el-table :data="gridData"  align="left">
               <el-table-column property="compName" :label="$t('table.compName')" width="150"></el-table-column>
               <el-table-column property="deployFileName" :label="$t('table.deployFileName')" width="200"></el-table-column>
-              <el-table-column :type="scope.row.deployState | statusFilter2" property="deployState" :label="$t('table.deployState')"></el-table-column>
+              <el-table-column :type="scope.row.deployState" property="deployState" :label="$t('table.deployState')"></el-table-column>
             </el-table>
           </el-dialog>
         </template>
       </el-table-column>
       <el-table-column align="center" :label="$t('table.actions')" width="145" class-name="small-padding fixed-width">
         <template slot-scope="scope">
-          <el-button size="mini" type="success">部署</el-button>
+          <el-button size="mini" type="success" :id="scope.row.online"
+                     @click="deployDevice(scope.row)">部署</el-button>
         </template>
       </el-table-column>
 
@@ -62,6 +63,7 @@
 
 <script>
   import { getDevices } from '@/api/device'
+  import { doDeploy } from '@/api/deploy'
   import waves from '@/directive/waves' // 水波纹指令
 
   /* eslint-disable */
@@ -81,6 +83,7 @@
           password: ''
         },
         proId: '',
+        deployPlanId: '',       //所选部署设计的id
         listLoading: true,
         dialogTableVisible: false,
         gridData: [{
@@ -106,7 +109,12 @@
       this.userData.username = this.getCookie('username')
       this.userData.password = this.getCookie('password')
       this.proId = this.getCookie('projectId')
+
+      this.deployPlanId = this.$route.params.id
       this.getList()
+      /*setInterval(() => {
+        this.getList()
+      }, 10 * 1000);*/
     },
     methods: {
       getList() {
@@ -115,14 +123,82 @@
           this.list = response.data.data
           this.listLoading = false
         })
+      },
+      deployDevice: function (row) {
+        let username = this.getCookie('username');
+        let password = this.getCookie('password');
+
+        let id = row.id;
+        //alert(id);
+        let qs = require('qs');
+
+        let online = false;
+
+        for (let i = 0; i < this.list.length; i++) {
+          if (this.list[i].id === id) {
+            online = this.list[i].online;
+            break;
+
+          }
+        }
+
+        if (online) {
+          let msg = "您确定部署吗？";
+          if (confirm(msg) === true) {
+
+            doDeploy(this.userData, this.deployPlanId, id).then(() => {
+              this.dialogFormVisible = false
+              this.$notify({
+                title: '成功',
+                message: '开始部署',
+                type: 'success',
+                duration: 2000
+              })
+              setInterval(() => {
+                this.getList()
+              }, 10 * 1000);
+
+              this.setProjectNum(this.listLength)
+            }).catch(err => {
+              console.log("提示---------");
+              console.log(err.response.data.data);
+              if(err.response.data.data.length != 0){
+                this.$notify({
+                  title: '失败',
+                  message: err.response.data.data,
+                  type: 'error',
+                  duration: 2000
+                })
+              }
+            })
+
+
+          } else {
+            return false;
+          }
+        } else {
+          //layer.msg("设备离线！");
+          this.$message({
+            message: '设备离线!',
+            type: 'warning'
+          })
+        }
+
+
+
       }
     },
     computed: {
       listA: function () {
         let self = this;
-        return self.list.filter(function (item) {
-          return item.name.toLowerCase().indexOf(self.searchQuery.toLowerCase()) !== -1;
-        })
+        console.log("list---------------");
+        console.log(self.list);
+        if(self.list !== null){
+          return self.list.filter(function (item) {
+            return item.name.toLowerCase().indexOf(self.searchQuery.toLowerCase()) !== -1;
+          })
+        }
+
       },
       listenProjectId () {
         return this.$store.state.app.projectId
